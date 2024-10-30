2ebb8d38b6a270052a0c83026efbb38982939d02
Artiom1999 <artiombazeliuk@gmail.com>
2024-10-30 21:44:52 +0200

    Initial commint

---
+++ pirma-pamoka/script.js
@@ -0,0 +1,22 @@
+//log
+console.log(2);
+console.log("Artiom");
+console.log(10000);
+//error
+console.error("Klaida");
+//clear
+console.clear();
+//math
+console.log(1 + 1);
+// Info
+console.info("Informacija");
+//Table
+console.table(["apple", "banana", "orange"]);
+//Warn
+console.warn("Ispejimas");
+//time (padeda pamotuoti kiek laiko uztrunka vykdyti koda)
+console.time();
+//Kodas kuri skaiciuoja
+console.timeEnd();
+
+//document.getElementById("heading").innerHTML = "Hello world!";
