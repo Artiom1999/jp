015880f2dafea2020c0f572db1629a50502b6ea8
Artiom1999 <artiombazeliuk@gmail.com>
2024-11-04 21:52:00 +0200

    trecia-pamoka

---
+++ trecia-pamoka/script.js
@@ -0,0 +1,65 @@
+// 1. Uždavinys: Skaičių suma
+// Sukurk tris kintamuosius su skaičiaus tipo reikšmėmis. Apskaičiuok jų sumą ir priskirk ją naujam kintamajam. Atspausdink rezultatą.
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+
+// let suma = a + b + c;
+
+// console.log(suma);
+
+// 2. Uždavinys: Stačiakampio plotas
+// Sukurk du kintamuosius - ilgis ir plotis, priskirk jiems bet kokias reikšmes ir apskaičiuok stačiakampio plotą.
+
+//let ilgis = 5;
+//let plotis = 10;
+
+//console.log(ilgis * plotis);
+
+// 3. Uždavinys: Kvadrato plotas
+// Sukurk kintamąjį ir priskirk jam bet kokį skaičių. Apskaičiuok kvadrato plotą ir jį atspausdink.
+
+//let Ilgis = 50;
+//let plotis = 50;
+
+//console.log(Ilgis * plotis);
+
+// 4. Uždavinys: Vardo pasisveikinimas
+// Sukurk kintamąjį vardas ir priskirk jam kokį nors vardą. Programa turi pasisveikinti su tuo vardu. Pvz.: “Labas, Vardeni”.
+
+//let vardas = 'Artiom';
+
+//console.log('labas: ' + vardas);
+
+// 5. Uždavinys: Dvigubas skaičius
+// Sukurk kintamąjį skaicius, jam priskirk bet kokią skaitinę reikšmę. Programa turi atspausdinti dvigubą šio skaičiaus reikšmę.
+
+let skaicius = 2;
+
+console.log('2' + '2');
+
+// 6. Uždavinys: Kūno masės indeksas
+// Sukurk kintamuosius ūgis ir svoris. Parašyk programą, kuri apskaičiuoja kūno masės indeksą.
+
+let ugis = 176;
+let svoris = 90;
+
+// 7. Uždavinys: Draugų skaičius
+// Sukurk kintamąjį drauguSkaicius, kuriame bus saugomas tavo draugų skaičius. Parašyk programą, kuri prideda 5 naujus draugus ir atspausdina bendrą draugų skaičių.
+
+// 8. Uždavinys: Kavos užsakymas
+// Sukurk kintamuosius kavosKiekis ir kavosKaina. Parašyk programą, kuri suskaičiuoja kavos užsakymo sumą ir ją atspausdina.
+
+// 9. Uždavinys: PVM skaičiavimas
+// Sukurk kintamuosius kainaBePVM ir pvm, kurie išsaugos prekės kainą ir PVM procentą (pvz., 21). Programa turi apskaičiuoti kainą su PVM.
+
+// 10. Uždavinys: Automobilio kuro sąnaudos
+// Sukurk kintamuosius nuvaziuotiKm, kuroSanaudos ir kuroKaina. Parašyk programą, kuri apskaičiuoja kiek kainuos nukeliauti nuvaziuotiKm su šiuo automobiliu.
+
+// 11. Uždavinys: Metų skaičius mėnesiais.
+// Sukurk kintamąjį metai, kuriame būtų saugomas skaičius, kiek metų. Programa turi apskaičiuoti, kiek tai sudaro mėnesių.
+
+// 12. Uždavinys: Temperatūros perskaičiavimas
+// Parašyk programą, kuri konvertuoja temperatūrą iš Celsijaus į Farenheito skalę. Sukurk kintamąjį temperaturaC ir priskirk jam bet kokią reikšmę,
+// o tada apskaičiuok temperatūrą Farenheito skalėje. Formulė: F = C * 9/5 + 32
